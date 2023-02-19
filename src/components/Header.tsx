bbd7098b98c020b0250673c24976e836e2cfab9c
Brianjaden <brianjaden45@gmail.com>
2023-02-19 15:16:37 -0600

    adding nextauth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,41 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Header() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <div className="h-14 border-b flex items-center justify-between px-10">
+          <div>
+            <h1 className="font-semibold text-2xl">Notelist</h1>
+          </div>
+          <div>
+            <button
+              onClick={() => signIn}
+              className="border rounded-full px-6 py-2 font-semibold border-gray-800"
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
-    <div className="h-14 border-b flex items-center justify-between px-10">
-      <div>
-        <h1 className="font-semibold text-2xl">Notelist</h1>
+    <>
+      <div className="h-14 border-b flex items-center justify-between px-10">
+        <div>
+          <h1 className="font-semibold text-2xl">Notelist</h1>
+        </div>
+        <div>
+          <button
+            onClick={() => signOut}
+            className="border rounded-full px-6 py-2 font-semibold border-gray-800"
+          >
+            Sign In
+          </button>
+        </div>
       </div>
-      <div>
-        <button className="border rounded-full px-6 py-2 font-semibold border-gray-800">
-          Sign in
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
-import { Inter } from '@next/font/google'
+import { Inter } from "@next/font/google";
+import { SessionProvider } from "next-auth/react";
 
-const inter = Inter ({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
-export default function App({
-Component, pageProps }:AppProps) {
-return (
+export default function App({ Component, pageProps }: AppProps) {
+  return (
     <main className={inter.className}>
+      <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
+      </SessionProvider>
     </main>
-);
+  );
 }
