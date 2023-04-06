a9b3508d0dd161d5d038581571318023201340e4
Brianmulinge <brianjaden45@gmail.com>
2023-04-06 18:15:22 -0500

    remaking the landing page

---
+++ src/components/Editor.tsx
@@ -0,0 +1,32 @@
+// Import React dependencies.
+import React, { useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+
+// TypeScript users only add this code
+import { BaseEditor, Descendant } from 'slate'
+import { ReactEditor } from 'slate-react'
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+
+const initialValue = []
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()))
+  // Render the Slate context.
+  return <Slate editor={editor} value={initialValue} />
+}
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import Header from "../components/Header";
-import Link from "next/link";
-import { StarIcon } from "@heroicons/react/24/outline";
 import { signIn } from "next-auth/react";
 
 function landing() {
   return (
-    <div className="">
-      <Header />
-      <div className="flex h-screen w-full flex-col text-center items-center justify-center">
-        <Link href="https://github.com/Brianmulinge/notelist">
-          <div className="flex rounded-full border items-center px-6 py-2">
-            <StarIcon className="h-8 w-8 lg:h-12 lg:w-12" />
-            <h1 className="font-semibold text-base md:text-lg lg:text-xl">
-              Star on Github
-            </h1>
-          </div>
-        </Link>
-        <h1 className="font-bold text-3xl py-4 lg:text-7xl md:text-5xl">
-          Notelist, an auto delete note taking app.
-        </h1>
+    <section className="h-screen w-screen flex flex-col justify-between">
+      <section className="flex justify-between items-center h-14 px-4 border-b">
+        <h1 className="font-semibold text-xl">Notelist</h1>
         <button
           onClick={() => signIn()}
-          className="border rounded-full px-6 py-2 font-semibold border-gray-800 text-base md:text-lg lg:text-xl"
+          className="border px-4 py-2 rounded-xl"
         >
-          Sign in to get started.
+          SignIn
         </button>
-      </div>
-    </div>
+      </section>
+      <section className="text-center mx-4 space-y-8">
+        <div className="">
+          <h1 className="font-bold text-3xl md:text-5xl lg:7xl">
+            Notelist, the best way to keep your notes in check
+          </h1>
+        </div>
+        <div className="border rounded-lg h-40">
+          <h1>Image</h1>
+        </div>
+      </section>
+      <section className="h-14 w-full flex flex-col justify-center items-center border-t  inset-x-0 bottom-0">
+      <h1>Created By Brian Mulinge</h1>
+        <h1>Notelist All Copyrights Reserved @2023</h1>
+      </section>
+    </section>
   );
 }
 
