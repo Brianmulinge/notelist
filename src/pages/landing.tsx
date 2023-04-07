13bf464bb8c9d4920f551dabfb32de71703c6953
Brianmulinge <brianjaden45@gmail.com>
2023-04-07 07:53:49 -0500

    added svg to the landing age

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,31 +1,47 @@
 import React from "react";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
+import pic from "../assets/notes.svg";
+import { motion } from "framer-motion";
 
 function landing() {
   return (
-    <section className="h-screen w-screen flex flex-col justify-between">
+    <section
+    style={{}}
+    className="h-screen w-screen flex flex-col justify-between">
       <section className="flex justify-between items-center h-14 px-4 border-b">
-        <h1 className="font-semibold text-xl">Notelist</h1>
+        <h1 className="font-semibold text-xl md:text-2xl md:font-bold lg:text-3xl">
+          Notelist
+        </h1>
         <button
           onClick={() => signIn()}
           className="border px-4 py-2 rounded-xl"
         >
-          SignIn
+          Sign In
         </button>
       </section>
       <section className="text-center mx-4 space-y-8">
-        <div className="">
-          <h1 className="font-bold text-3xl md:text-5xl lg:7xl">
-            Notelist, the best way to keep your notes in check
+        <motion.div initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }} className="">
+          <h1 className="font-bold text-5xl md:text-7xl lg:8xl text-transparent bg-clip-text bg-gradient-to-br from-gray-950 to-gray-400">
+            Notelist
           </h1>
-        </div>
-        <div className="border rounded-lg h-40">
-          <h1>Image</h1>
-        </div>
+          <h1 className="font-bold text-3xl md:text-5xl lg:7xl text-transparent bg-clip-text bg-gradient-to-br from-gray-400 to-gray-950">
+            The best way to keep your notes in check
+          </h1>
+        </motion.div>
+        <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="flex h-auto w-full justify-center">
+          <Image src={pic} className="p-8" alt="notes_svg" />
+        </motion.div>
       </section>
-      <section className="h-14 w-full flex flex-col justify-center items-center border-t  inset-x-0 bottom-0">
-      <h1>Created By Brian Mulinge</h1>
-        <h1>Notelist All Copyrights Reserved @2023</h1>
+      <section className="border-t h-14 flex flex-col items-center justify-center">
+        <h1 className="">Notelist, All Rights Reserved</h1>
+        <h1 className="text-sm">@2023</h1>
       </section>
     </section>
   );
